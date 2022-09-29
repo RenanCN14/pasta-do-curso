48782c7ed16b518408d231ed479c396485216149
RenanCN14 <renan14032000@gmail.com>
2022-09-28 21:09:07 -0300

    JS do incio

---
+++ js_curso/comentarios.js
@@ -0,0 +1,58 @@
+// AULA 1
+
+// Para comentar uma linha
+
+/*
+    Para comentar um bloco de texto
+    com mais linhas.
+*/
+
+console.log("Texto aleatorio") //Comentario na mesma linha que será executada
+
+
+// AULA 2
+
+console.log(`Renan ${1+1}`)
+
+
+// AULA 3
+
+console.log(true)
+console.log(false)
+
+
+// AULA 4
+console.log(undefined) //Algo que não existe
+console.log(null) //Algo que existe mas não tem nada dentro
+
+
+// AULA 5
+
+console.log({
+    name: "Renan",
+    idade: 22,
+    andar: function(){
+        console.log('andar')
+    }
+})
+
+
+//AULA 6
+
+console.log([
+    "Leite", "Ovos", 2, 3
+])
+
+// AULA 7
+
+var clima = "Quente"
+    console.log(clima)
+
+let clima2 = "Frio"
+    console.log(clima2)
+
+const clima3 = "Morno"
+    console.log(clima3)
+
+
+
