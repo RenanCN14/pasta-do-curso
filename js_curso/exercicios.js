4c6873d1ac54b371665cf612e551249985f76409
RenanCN14 <renan14032000@gmail.com>
2022-09-29 22:45:20 -0300

    iniciando_JS

---
+++ js_curso/exercicios.js
@@ -0,0 +1,37 @@
+let weight
+
+// console.log(typeof weight)
+
+let name2 = "Renan"
+let age = 22
+let stars = 4.5
+let isSubscribe = false
+
+let student = {
+    name1 : "Renan Campos",
+    age : 22,
+    stars : 4.5,
+    isSubscribe : false,
+    weight : 80
+}
+
+// console.log(`${student.name1} tem ${student.age} de idade e pesa ${student.weight} quilos`)
+
+studens = []
+
+students = [
+    student
+]
+// console.log(students[0])
+
+const jhon ={
+    name : "Jhon",
+    age : 23,
+    weight :73.2,
+    isSubscribe : true,
+}
+students[1] = [
+    jhon
+]
+
+console.log(students)
---
+++ js_curso/scripts.js
@@ -0,0 +1,22 @@
+const person = {
+    name : "Jonh",
+    age : 30,
+    weigght :88.6,
+    isAdmin: true
+}
+
+console.log(`${person.name} tem ${person.age} anos de idade`)
+
+
+
+const animals = [
+    "Lion", 
+    "MOnkey", 
+    "Cat",
+    {
+        name : "CAT",
+        age: 3
+    }
+]
+
+console.log(animals[3].name)
